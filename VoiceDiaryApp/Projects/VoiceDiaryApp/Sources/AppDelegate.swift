ae7a162744e8e058cb104fbaacaa4a5311eb01a3
ahra1221 <lizze011221@gmail.com>
2024-11-22 17:50:04 +0900

    #3 setting: uikit 기반 코드 작성을 위한 파일  생성

---
+++ VoiceDiaryApp/Projects/VoiceDiaryApp/Sources/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  VoiceDiaryApp
+//
+//  Created by 고아라 on 11/22/24.
+//
+
+import Foundation
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+        return true
+    }
+}
---
+++ VoiceDiaryApp/Projects/VoiceDiaryApp/Sources/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  VoiceDiaryApp
+//
+//  Created by 고아라 on 11/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .purple
+    }
+}
