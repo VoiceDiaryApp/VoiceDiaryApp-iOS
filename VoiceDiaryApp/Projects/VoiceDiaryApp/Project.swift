f0a3ae33542372fef9a92b1a6982ed7c4e7d8b7b
ahra1221 <lizze011221@gmail.com>
2024-11-29 16:17:01 +0900

    #5 feat: 폰트 추가 및 설정

--- VoiceDiaryApp/Projects/VoiceDiaryApp/Project.swift
+++ VoiceDiaryApp/Projects/VoiceDiaryApp/Project.swift
@@ -16,7 +16,12 @@ let project = Project(
                     ],
                     "NSMicrophoneUsageDescription": "음성 일기 작성을 위해 마이크 접근이 필요합니다.",
                     "NSPhotoLibraryAddUsageDescription": "사진을 저장하기 위해 앨범 접근 권한이 필요합니다.",
-                    "NSSpeechRecognitionUsageDescription": "음성 명령 입력을 위해 음성 인식 권한이 필요합니다."
+                    "NSSpeechRecognitionUsageDescription": "음성 명령 입력을 위해 음성 인식 권한이 필요합니다.",
+                    "UIAppFonts": [
+                        "Pretendard-Medium.otf",
+                        "Pretendard-SemiBold.otf",
+                        "Pretendard-Bold.otf"
+                    ]
                 ]
             ),
             sources: ["Sources/**"],
