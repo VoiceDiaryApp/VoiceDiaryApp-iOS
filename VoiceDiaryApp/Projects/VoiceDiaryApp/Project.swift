1e502b0a89baafad999c892d0376f6e6fe646f49
hoyeon <fnfn0901@sookmyung.ac.kr>
2024-12-09 16:47:02 +0900

    feat: 폰트 추가 및 fs 캘린더 추가

--- VoiceDiaryApp/Projects/VoiceDiaryApp/Project.swift
+++ VoiceDiaryApp/Projects/VoiceDiaryApp/Project.swift
@@ -14,6 +14,21 @@ let project = Project(
                         "UIColorName": "",
                         "UIImageName": "",
                     ],
+                    "UIAppFonts": [
+                        "Fonts/NotoSans-Bold.ttf",
+                        "Fonts/Pretendard-Black.ttf",
+                        "Fonts/Pretendard-Bold.ttf",
+                        "Fonts/Pretendard-ExtraBold.ttf",
+                        "Fonts/Pretendard-ExtraLight.ttf",
+                        "Fonts/Pretendard-Light.ttf",
+                        "Fonts/Pretendard-Medium.ttf",
+                        "Fonts/Pretendard-Regular.ttf",
+                        "Fonts/Pretendard-SemiBold.ttf",
+                        "Fonts/Pretendard-Thin.ttf",
+                        "Fonts/Roboto-Black.ttf",
+                        "Fonts/Roboto-Bold.ttf",
+                        "Fonts/Roboto-Regular.ttf"
+                    ]
                 ]
             ),
             sources: ["Sources/**"],
--- VoiceDiaryApp/Tuist/Package.swift
+++ VoiceDiaryApp/Tuist/Package.swift
@@ -21,5 +21,14 @@ let package = Package(
         // Add your own dependencies here:
         // .package(url: "https://github.com/Alamofire/Alamofire", from: "5.0.0"),
         // You can read more about dependencies here: https://docs.tuist.io/documentation/tuist/dependencies
+    ],
+    targets: [
+        .target(
+            name: "VoiceDiaryApp",
+            dependencies: [
+                "SnapKit",
+                "FSCalendar"
+            ]
+        )
     ]
 )
