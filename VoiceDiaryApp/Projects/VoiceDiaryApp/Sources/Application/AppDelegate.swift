e04f7ed23c2cc267cefc54bb6f8e04b673b9b908
hoyeon <fnfn0901@sookmyung.ac.kr>
2024-12-13 10:33:40 +0900

    #6 chore: 초기 화면 ViewController로 복구

--- VoiceDiaryApp/Projects/VoiceDiaryApp/Sources/Application/AppDelegate.swift
+++ VoiceDiaryApp/Projects/VoiceDiaryApp/Sources/Application/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
         let window = UIWindow(frame: UIScreen.main.bounds)
-        let rootVC = DiaryViewController(viewModel: DiaryViewModel())
+        let rootVC = ViewController()
         window.rootViewController = rootVC
         window.makeKeyAndVisible()
         self.window = window
