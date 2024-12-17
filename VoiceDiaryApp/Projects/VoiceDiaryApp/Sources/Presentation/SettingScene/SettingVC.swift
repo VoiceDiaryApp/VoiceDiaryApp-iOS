ea9b980916c2b4298a5ad57f66c1b622cf17955d
hoyeon <fnfn0901@sookmyung.ac.kr>
2024-12-17 15:20:32 +0900

    #7 fix: 세팅뷰모델 제거

--- VoiceDiaryApp/Projects/VoiceDiaryApp/Sources/Presentation/SettingScene/SettingVC.swift
+++ VoiceDiaryApp/Projects/VoiceDiaryApp/Sources/Presentation/SettingScene/SettingVC.swift
@@ -6,13 +6,8 @@
 //
 
 import UIKit
-import Combine
 
 class SettingVC: UIViewController {
-    
-    // MARK: - Properties
-    private let viewModel = Diary2ViewModel()
-    private var cancellables = Set<AnyCancellable>()
     
     // MARK: - View
     private let settingView = SettingView()
@@ -25,11 +20,5 @@ class SettingVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.isHidden = true
-        bindViewModel()
-    }
-    
-    // MARK: - ViewModel Binding
-    private func bindViewModel() {
-        
     }
 }
