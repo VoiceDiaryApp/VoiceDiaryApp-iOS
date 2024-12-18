ff141f60b7f572388ebe62181e7a7f46837932ef
ahra_ <ahravelop@gmail.com>
2024-12-18 13:12:20 +0900

    #17 feat: GoogleGenerativeAI 패키지 추가

--- VoiceDiaryApp/Tuist/Package.swift
+++ VoiceDiaryApp/Tuist/Package.swift
@@ -15,8 +15,8 @@ import PackageDescription
 let package = Package(
     name: "VoiceDiaryApp",
     dependencies: [
-        .package(url: "https://github.com/SnapKit/SnapKit", from: "5.7.1")
-
+        .package(url: "https://github.com/SnapKit/SnapKit", from: "5.7.1"),
+        .package(url: "https://github.com/google/generative-ai-swift", from: "0.5.6")
         // Add your own dependencies here:
         // .package(url: "https://github.com/Alamofire/Alamofire", from: "5.0.0"),
         // You can read more about dependencies here: https://docs.tuist.io/documentation/tuist/dependencies
@@ -25,7 +25,8 @@ let package = Package(
         .target(
             name: "VoiceDiaryApp",
             dependencies: [
-                "SnapKit"
+                "SnapKit",
+                "GoogleGenerativeAI"
             ]
         )
     ]
