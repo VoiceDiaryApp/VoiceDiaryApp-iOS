0cdcd8212c8a9e8c955ad800011085882e6b3c6c
ahra_ <ahravelop@gmail.com>
2024-12-19 14:24:07 +0900

    #19 feat: 사용자 일기 기반 prompt 생성 및 gemini 답장 구현

--- VoiceDiaryApp/Projects/VoiceDiaryApp/Sources/Presentation/DiaryScene/DiaryVM.swift
+++ VoiceDiaryApp/Projects/VoiceDiaryApp/Sources/Presentation/DiaryScene/DiaryVM.swift
@@ -4,13 +4,17 @@
 //
 //  Created by 고아라 on 12/9/24.
 //
+import Foundation
 
 import Combine
+import GoogleGenerativeAI
 
 final class DiaryVM: ViewModel {
     
     private var cancellables = Set<AnyCancellable>()
     private var recordingContent: String = ""
+    private let model = GenerativeModel(name: "gemini-1.5-flash-latest",
+                                        apiKey: Config.apiKey)
     
     struct Input {
         let onRecording: PassthroughSubject<String, Never>
@@ -25,7 +29,6 @@ final class DiaryVM: ViewModel {
     
     func transform(input: Input) -> Output {
         let output = Output()
-        self.bindOutput(output: output)
         
         input.onRecording
             .sink { value in
@@ -35,7 +38,7 @@ final class DiaryVM: ViewModel {
         
         input.tapRecordEnd
             .sink { _ in
-                print("✅✅recordingcontent✅✅")
+                print("🥹🥹🥹🥹")
                 print(self.recordingContent)
                 output.recordContent.send(self.recordingContent)
             }
@@ -43,15 +46,45 @@ final class DiaryVM: ViewModel {
         
         input.tapDrawEnd
             .sink { value in
-                print("✅✅selectemotion✅✅")
-                print(value)
+                print("🥹🥹🥹🥹")
+                print(self.recordingContent)
+                print(output.recordContent.value)
+                self.generatePrompt(content: output.recordContent.value,
+                                    emotion: value,
+                                    output: output)
             }
             .store(in: &cancellables)
         
         return output
     }
+}
+
+private extension DiaryVM {
     
-    private func bindOutput(output: Output) {
-        
+    func generateGeminiLetter(prompt: String,
+                              output: Output) async {
+        let prompt = prompt
+        do {
+            let response = try await model.generateContent(prompt)
+            let letterContent = response.candidates.first?.content.parts.first?.text ??  "깨비가 답장을 쓰지 못했어요. \n다시 시도해주세요!"
+            print("🎃🎃outputcontent🎃🎃")
+            print(letterContent)
+            print("🎃🎃🎃🎃")
+            output.geminiLetter.send(letterContent)
+        } catch {
+            print("Error: \(error)")
+        }
+    }
+    
+    func generatePrompt(content: String,
+                        emotion: Emotion,
+                        output: Output) {
+        let generatedPrompt = "\"" + content + emotion.emotionToPrompt + "\"에 대한 답장을 써줘. 나는 일기를 쓰는 아이고 우리는 친구야. 내가 쓴 일기를 너한테 보내면, 도깨비가 귀여운 말투로 나에게 답장을 써줘. 답장은 이모티콘 없이 편안하고 친근한 말투로, 내용은 조금 길고 따뜻한 감정이 담겨 있으면 좋겠어."
+        print("💭💭generatedPrompt💭💭")
+        print(generatedPrompt)
+        Task {
+            await generateGeminiLetter(prompt: generatedPrompt,
+                                       output: output)
+        }
     }
 }
