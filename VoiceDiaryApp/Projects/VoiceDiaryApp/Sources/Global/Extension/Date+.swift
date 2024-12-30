e3adb19dfb9126041e25b68276b47156a3895b40
ahra <ahravelop@gmail.com>
2024-12-30 15:18:36 +0900

    #33 feat: date를 string으로 바꾸는 함수 추가

--- VoiceDiaryApp/Projects/VoiceDiaryApp/Sources/Global/Extension/Date+.swift
+++ VoiceDiaryApp/Projects/VoiceDiaryApp/Sources/Global/Extension/Date+.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 extension Date {
+    
     func toLocalTime() -> Date {
         let timeZone = TimeZone.current
         let seconds = TimeInterval(timeZone.secondsFromGMT(for: self))
@@ -18,5 +19,13 @@ extension Date {
         let timeZone = TimeZone.current
         let seconds = -TimeInterval(timeZone.secondsFromGMT(for: self))
         return Date(timeInterval: seconds, since: self)
+    }
+    
+    func dateToString() -> String {
+        let koreanTimeZone = TimeZone(identifier: "Asia/Seoul")!
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        formatter.timeZone = koreanTimeZone
+        return formatter.string(from: self)
     }
 }
