3d405dff0fcb210c8180dfd92f371f43c99f7474
ahra <ahravelop@gmail.com>
2025-01-02 12:18:40 +0900

    #42 feat: combine을 활용한 tapGesturePublisher 구현

---
+++ VoiceDiaryApp/Projects/VoiceDiaryApp/Sources/Global/Extension/UITapGestureRecognizer+.swift
@@ -0,0 +1,60 @@
+//
+//  UITapGestureRecognizer+.swift
+//  VoiceDiaryApp
+//
+//  Created by ahra on 1/2/25.
+//
+
+import UIKit
+
+import Combine
+
+extension UITapGestureRecognizer {
+    
+    struct GesturePublisher<TapRecognizer: UITapGestureRecognizer>: Publisher {
+        typealias Output = TapRecognizer
+        typealias Failure = Never
+        
+        private let recognizer: TapRecognizer
+        private let view: UIView
+        
+        init(recognizer: TapRecognizer, view: UIView) {
+            self.recognizer = recognizer
+            self.view = view
+        }
+        
+        func receive<S>(subscriber: S) where S: Subscriber, Never == S.Failure, TapRecognizer == S.Input {
+            let subscription = GestureSubscription(
+                subscriber: subscriber,
+                recognizer: recognizer,
+                view: view
+            )
+            subscriber.receive(subscription: subscription)
+        }
+    }
+}
+
+extension UITapGestureRecognizer {
+    final class GestureSubscription<S: Subscriber, TapRecognizer: UITapGestureRecognizer>: Subscription
+    where S.Input == TapRecognizer {
+        private var subscriber: S?
+        private let recognizer: TapRecognizer
+        
+        init(subscriber: S, recognizer: TapRecognizer, view: UIView) {
+            self.subscriber = subscriber
+            self.recognizer = recognizer
+            recognizer.addTarget(self, action: #selector(eventHandler))
+            view.addGestureRecognizer(recognizer)
+        }
+        
+        func request(_ demand: Subscribers.Demand) {}
+        
+        func cancel() {
+            subscriber = nil
+        }
+        
+        @objc func eventHandler() {
+            _ = subscriber?.receive(recognizer)
+        }
+    }
+}
