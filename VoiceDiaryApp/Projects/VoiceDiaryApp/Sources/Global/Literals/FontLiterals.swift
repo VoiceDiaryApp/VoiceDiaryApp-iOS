560414418a6893596a5ef4d7a2ec1a973e2c91c9
ahra <ahravelop@gmail.com>
2025-01-02 12:26:59 +0900

    #42 feat: 동적 폰트 기기대응을 위해 각 폰트별 textstyle 설정

--- VoiceDiaryApp/Projects/VoiceDiaryApp/Sources/Global/Literals/FontLiterals.swift
+++ VoiceDiaryApp/Projects/VoiceDiaryApp/Sources/Global/Literals/FontLiterals.swift
@@ -15,12 +15,22 @@ enum FontType: String {
     case RobotobBold = "Roboto-Bold"
     case RobotoRegular = "Roboto-Regular"
     case GangwonEduSaeeum = "GangwonEduSaeeum"
+    
+    var textStyle: UIFont.TextStyle {
+        switch self {
+        case .PretandardBold, .RobotobBold: return .title1
+        case .PretandardSemiBold: return .title2
+        case .PretandardMedium: return .title3
+        case .PretandardRegular, .RobotoRegular: return .caption1
+        case .GangwonEduSaeeum: return .title1
+        }
+    }
 }
 
 extension UIFont {
     
     static func fontGuide(type: FontType, size: CGFloat) -> UIFont {
-        let font = UIFont(name: type.rawValue, size: size)!
+        let font = UIFont(name: type.rawValue, size: UIFont.preferredFont(forTextStyle: type.textStyle).pointSize)!
         return font
     }
 }
